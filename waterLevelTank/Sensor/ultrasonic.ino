d2bfdea6e821f4b009a5e1022924a3b51ced3b20
AbdulRahman7861234 <130071516+AbdulRahman7861234@users.noreply.github.com>
2023-07-14 10:35:57 +0100

    Update ultrasonic.ino

--- waterLevelTank/Sensor/ultrasonic.ino
+++ waterLevelTank/Sensor/ultrasonic.ino
@@ -1,8 +1,31 @@
 const int trigPin = 9;
 const int echoPin = 10;
-const int num_samples = 10;
+const int bufferSize = 10;
 
-void init_ultrasonics() {
+int buffer[bufferSize];
+int writeIndex = 0;
+int readIndex = 0;
+
+void Ultrasonic_pins() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void get_readings() {
+  float distance = measureDistance();
+  put(distance);
+  
+  if (writeIndex == 0 && readIndex != 0) {
+    float average = get_average_distance();
+    Serial.print("Average: ");
+    Serial.println(average);
+  }
+  
+  delay(100); // Delay for 1 second between measurements
+}
+
+void init_Ultrasonics() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
@@ -13,21 +36,31 @@ float measureDistance() {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
+  
   long duration = pulseIn(echoPin, HIGH);
   float distance = duration * 0.034 / 2;
-
-  return distance; // Return the measured distance
+  
+  return distance;
 }
 
-float get_average_distance(int num_samples) {
-  float average = 0;
-  for (int i = 0; i < num_samples; i++) {
-    average += measureDistance();
-    delay(100); // Delay for 100ms between measurements
+void put(float item) {
+  buffer[writeIndex] = item;
+  writeIndex = (writeIndex + 1) % bufferSize;
+  if (writeIndex == readIndex) {
+    readIndex = (readIndex + 1) % bufferSize;
   }
-   Serial.print("Distance: ");
-  Serial.print(average / num_samples);
-  Serial.println(" cm");
-  return average / num_samples;
+}
+
+float get_average_distance() {
+  float sum = 0;
+  int count = 0;
+  
+  int currentIndex = readIndex;
+  while (currentIndex != writeIndex) {
+    sum += buffer[currentIndex];
+    count++;
+    currentIndex = (currentIndex + 1) % bufferSize;
+  }
+  
+  return sum / count;
 }
