045240f9d6a03e98712939dc649bf70aac5ba859
AbdulRahman7861234 <130071516+AbdulRahman7861234@users.noreply.github.com>
2023-07-14 10:36:42 +0100

    Update Sensor.ino

--- waterLevelTank/Sensor/Sensor.ino
+++ waterLevelTank/Sensor/Sensor.ino
@@ -1,8 +1,6 @@
 #define ON true
 #define OFF false
 
-
-
 typedef struct WaterLevel_t {
   bool pumpOff;
   int pumpTimer;
@@ -17,23 +15,32 @@ typedef enum WaterLevelTankStates_t {
   PUMPING_EMPTY,
   PUMPING_FILL,
   DELAY,
-  //LIDOTT_MEASURE,
+  LIDOTT_MEASURE,
 } WaterLevelTankStates_t;
 
 WaterLevelTankStates_t state = IDLE;
 WaterLevelTankStates_t lastState = IDLE;
 bool newState = true;
+WaterLevelTankStates_t nextState = IDLE;
 
 WaterLevel_t waterLevelTank;
 
 int desiredReading = 50;
 float offset = 0.1; // Lower and upper bounds
 
+void setupTimer(WaterLevelTankStates_t a_nextState, int timer){
+  state = DELAY;
+  nextState = a_nextState;
+  waterLevelTank.delayTimer= timer;
+}
+
+
 void setup() {
   Serial.begin(9600);
   
-  init_ultrasonics();
+  init_Ultrasonics();
   init_pumps();
+  
 
   Serial.println("Init complete.");
 }
@@ -59,7 +66,7 @@ void loop() {
       }
 
       // Take an ultrasonic measurement
-      waterLevelTank.ultrasonicLevel = get_average_distance(num_samples);
+      waterLevelTank.ultrasonicLevel = get_average_distance();
 
       sprintf(temp, "Ultrasonic level: %f", waterLevelTank.ultrasonicLevel);
       Serial.println(temp);
@@ -80,13 +87,17 @@ void loop() {
       }
 
       // Take an ultrasonic measurement
-      waterLevelTank.ultrasonicLevel = get_average_distance(num_samples);
+      waterLevelTank.ultrasonicLevel = get_average_distance();
 
       // Determine if the tank needs to be emptied - Go to Pumping state if required
       if (waterLevelTank.ultrasonicLevel < 30) {
         pump_action(1, OFF);
         state = IDLE;
-      }
+      } else {
+        setupTimer(PUMPING_EMPTY, 50); 
+      } 
+
+
       break;
 
     case PUMPING_FILL:
@@ -95,37 +106,44 @@ void loop() {
         pump_action(2, ON);
       }
       
-      waterLevelTank.ultrasonicLevel = get_average_distance(num_samples);
+      waterLevelTank.ultrasonicLevel = get_average_distance();
 
       // Determine if the tank needs to be filled
-      if (waterLevelTank.ultrasonicLevel < desiredReading) {
-        pump_action(2, ON);
-      } else {
+      if (waterLevelTank.ultrasonicLevel >= desiredReading){
         // Tank reached or exceeded desired reading
         pump_action(2, OFF);
-        desiredReading += 10;
-        state = DELAY;
-        
         if (desiredReading > 50) {
           state = IDLE;
+        } else {
+          state = LIDOTT_MEASURE;
+          desiredReading += 10;
         }
-      }
+
+      } else {
+        setupTimer(PUMPING_FILL, 50);
+        }
       break;
 
     case DELAY:
       if (newState) {
         Serial.println("DELAY");
-        waterLevelTank.delayTimer = 0;
+      //  waterLevelTank.delayTimer = 0;
       } 
-      waterLevelTank.delayTimer += 1;
+      waterLevelTank.delayTimer -= 1;
 
       // Delay for 5 seconds (5000ms)
-      if (waterLevelTank.delayTimer == 50) {
+      if (waterLevelTank.delayTimer == 0) {
         // Go back to filling up tank
-        state = PUMPING_FILL;
+        state = nextState;
       }
       break;
+      case LIDOTT_MEASURE:
+        setupTimer(LIDOTT_MEASURE, 5000);
+
+
+    break;
   }
-  
+
+  delay(1);
   newState = false;
 }
